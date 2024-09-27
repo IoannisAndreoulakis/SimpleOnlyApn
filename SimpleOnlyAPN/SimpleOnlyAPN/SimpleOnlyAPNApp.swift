39237805c4d3db07e76fb8f9706294bb027f0028
ioannis andreoulakis <i.andreoulakis@flexcar.gr>
2024-09-27 17:04:48 +0300

    Initial Commit

---
+++ SimpleOnlyAPN/SimpleOnlyAPN/SimpleOnlyAPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleOnlyAPNApp.swift
+//  SimpleOnlyAPN
+//
+//  Created by Ioannis Andreoulakis on 27/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleOnlyAPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleOnlyAPN/SimpleOnlyAPNTests/SimpleOnlyAPNTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleOnlyAPNTests.swift
+//  SimpleOnlyAPNTests
+//
+//  Created by Ioannis Andreoulakis on 27/9/24.
+//
+
+import Testing
+@testable import SimpleOnlyAPN
+
+struct SimpleOnlyAPNTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
